5ff9da2175924759c6d84f1f913e008d654f7181
Giovanna Leticia <158202419+letydrafts@users.noreply.github.com>
2025-05-13 15:26:59 -0300

    Update DeclaracaoController.php

--- SIGAC/app/Http/Controllers/DeclaracaoController.php
+++ SIGAC/app/Http/Controllers/DeclaracaoController.php
@@ -3,62 +3,90 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Declaracao;
+use App\Models\Aluno;
+use App\Models\Comprovante;
 
 class DeclaracaoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    
     public function index()
     {
-        //
+        $declarações = Declaracao::with(['aluno', 'comprovante'])->get();
+        return view('declaracoes.index', compact('declaracoes');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+
     public function create()
     {
-        //
+        $alunos = Aluno::all();
+        $comprovantes = Comprovante::all();
+        return view('declaracoes.create', compact('alunos', 'comprovantes'));
+    
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+        'hash'=>'request|string',
+        'data'=>'request|string',
+        'aluno_id'=>'request|exists:alunos,id',
+        'comprovante_id'=>'request|exists:comprovantes,id']);
+
+        Declaracao::create($request->all());
+
+        return redirect()->route('declaracoes.index')
+                        ->with('sucess', 'Declaração criada');
+                                        
+        ])
     }
 
-    /**
-     * Display the specified resource.
-     */
+   
     public function show(string $id)
     {
-        //
+        $declaracao = Declaracao::with(['aluno', 'comprovante'])->find($id);
+        return view('declaracao.show', compact('declaracao'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+    
     public function edit(string $id)
     {
-        //
+        $declaracao = Declaracao::find($id);
+        $alunos = Aluno::all();
+        $comprovantes = Comprovante::all();
+        return view('declaracoes.edit', compact('declaracao'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+   
     public function update(Request $request, string $id)
     {
-        //
+        $declaracao = Declaracao::find($id);
+
+        $validated = $request->validate([
+        'hash'=>'request|string',
+        'data'=>'request|string',
+        'aluno_id'=>'request|exists:alunos,id',
+        'comprovante_id'=>'request|exists:comprovantes,id']);
+
+        $declaracao->update($validated);
+
+        return redirect()->route('declaracoes.index')
+                        ->with('sucess', 'Declaração atualizada');
+        ]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    
     public function destroy(string $id)
     {
-        //
+        $declaracao = Declaracao::find($id);
+
+        if(isset($declaracao)){
+            $declaracao->delete();
+    
+             return redirect()->route('declaracoes.index')
+                        ->with('sucess', 'Declaração excluida');
+        }
+        return '<h1>Não foi possível excluir esse registro do sistema</h1>';
     }
 }
