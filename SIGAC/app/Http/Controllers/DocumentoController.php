7c6fae66467a993e1220b7934338d3fd4213c670
Giovanna Leticia <158202419+letydrafts@users.noreply.github.com>
2025-05-13 15:54:39 -0300

    Update DocumentoController.php

--- SIGAC/app/Http/Controllers/DocumentoController.php
+++ SIGAC/app/Http/Controllers/DocumentoController.php
@@ -4,61 +4,91 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Documento;
+use App\Models\Categoria;
+
 class DocumentoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    
     public function index()
     {
-        //
+       $documentos = Documento::with(['categoria'])->get();
+        return view('documentos.index', compact('documentos')); 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    
     public function create()
     {
-        //
+        $categorias = Categoria::all();
+        return view('documentos.create', compact('categorias'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    
     public function store(Request $request)
     {
-        //
+       $validated = $request->validate([
+        'url'=>'required|string',
+        'descricao'=>'required|string',
+        'horas_in'=>'required',
+        'status'=>'required|string',
+        'comentario'=>'required|string',
+        'horas_out'=>'required',
+        'categoria_id'=>'required|exists:categorias,id'
+       ]);
+
+        Documento::create($request->all());
+
+        return redirect()->route('documentos.index')
+                            ->with('sucess', 'Documento criado no sistema.');
     }
 
-    /**
-     * Display the specified resource.
-     */
+   
     public function show(string $id)
     {
-        //
+        $documento = Documento::with(['categoia'])->find($id);
+        return view('documentos.show', compact('documento'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+   
     public function edit(string $id)
     {
-        //
+        $documento = Documento::find($id);
+        $categorias = Categoria::all();
+        return view('categorias.edit', compact('categoria'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+   
     public function update(Request $request, string $id)
     {
-        //
+        $documento = Documento::find($id);
+        
+        $validated = $request->validate([
+        'url'=>'required|string',
+        'descricao'=>'required|string',
+        'horas_in'=>'required',
+        'status'=>'required|string',
+        'comentario'=>'required|string',
+        'horas_out'=>'required',
+        'categoria_id'=>'required|exists:categorias,id'
+       ]);
+
+        $documento>update($validated);
+
+        return redirect()->route('documentos.index')
+                        ->with('success', 'Documento atualizado');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+   
     public function destroy(string $id)
     {
-        //
+        $documento = Documento::find($id);
+
+        if(isset($documento)){
+            $documento->delete();
+
+            return redirect()->route('documentos.index')
+                    ->with('sucess', 'Documento excluido do sistema');
+        }
+        return '<h1>Não foi possível excluir esse documento do sistema</h1>';
     }
 }
